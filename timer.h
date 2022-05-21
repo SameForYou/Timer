4d9c66e8aa4eb04ac65f7833e4a1847270c29030
SameForYou <96946119+SameForYou@users.noreply.github.com>
2022-05-21 16:46:01 +0800

    Add files via upload

---
+++ timer.h
@@ -0,0 +1,20 @@
+#include <ctime>
+#include <iostream>
+#include <chrono>
+
+struct Timer {
+    std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
+    std::chrono::duration<float> duration;
+    Timer()
+    {
+        start = std::chrono::high_resolution_clock::now();
+    }
+    ~Timer()
+    {
+        end = std::chrono::high_resolution_clock::now();
+        duration = (end - start);
+        float ms = duration.count() * 1000.0f;
+        std::cout << "time took " << ms << "ms" << std::endl;
+    }
+};
+
